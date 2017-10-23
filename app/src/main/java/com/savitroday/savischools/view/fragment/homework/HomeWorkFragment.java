f3114bce70868e6050df6b9d825667ff45cbd673
harshita ahuja <aharsita48@gmail.com>
2017-10-23 11:35:18 +0530

    - reafctor fragments

--- app/src/main/java/com/savitroday/savischools/view/fragment/homework/HomeWorkFragment.java
+++ app/src/main/java/com/savitroday/savischools/view/fragment/homework/HomeWorkFragment.java
@@ -1,7 +1,5 @@
-package com.savitroday.savischools.view.fragment;
+package com.savitroday.savischools.view.fragment.homework;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
@@ -12,7 +10,6 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import com.savitroday.savischools.R;
 
--- app/src/main/java/com/savitroday/savischools/view/fragment/invoice/PendingInvoiceFragment.java
+++ app/src/main/java/com/savitroday/savischools/view/fragment/invoice/PendingInvoiceFragment.java
@@ -1,10 +1,9 @@
-package com.savitroday.savischools.view.fragment;
+package com.savitroday.savischools.view.fragment.invoice;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
