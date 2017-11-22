272c2b35e65188282ff599a6f3fb257a3b6cc054
Savitecheversion <amit.goyal@techeversion.com>
2017-11-22 12:27:46 +0530

    change

---
+++ app/src/main/java/com/savitroday/savischools/view/activity/ForgotPasswordActivity.java
@@ -0,0 +1,15 @@
+package com.savitroday.savischools.view.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.savitroday.savischools.R;
+
+public class ForgotPasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_password);
+    }
+}
